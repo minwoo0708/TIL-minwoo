4743288b877f9eca2c43d95e062e7e9d288011c0
mino_o.78 <s26010@gsm.hs.kr>
2026-03-12 17:12:56 +0900

    d

--- 교과 파이썬.py
+++ 교과 파이썬.py
@@ -1,14 +1,7 @@
-a=str(input('배송지를 입력하시오'))
-price= int(input('상품 가격을 입력하시오'))
-if a=='한국':
-    if price>=20000:
-        print('배송비는 무료')
-    else:
-        print('배송비는 3000원입니다')
+a=int(input('숫자를 입력하세요'))  
+for i in range(1,(a+1)):
+    print(i*a==i)
 
-else: 
-    if price>=100000:
-        print('배송비는 무료')
-    else:
-        print('배송비는 8000원')
-        
+    
+
+    i == b
